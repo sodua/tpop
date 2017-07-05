73ff5c125dc5ac133ce80b502ff06c27fc1ef5d7
sodua <cthildreth@gmail.com>
2017-07-04 17:44:49 -0700

    structs go outside of main, that took a while

--- binsearch.c
+++ binsearch.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 #define NELEMS(array) (sizeof(array) / sizeof(array[0]))
 
+typedef struct Nameval Nameval;
+struct Nameval {
+        char    *name;
+        int     value;
+        };
 
 int main(int argc, char **argv)
 {
-    double dbuf[100];
     int half;
-    half = lookup("Acirc", htmlchars, NELEMS(htmlchars));
-    printf("The HTML table has %d words\n", NELEMS(htmlchars));
-}
-
-/* lookup: binary search for name in tab; return index */
-int lookup(char *name, Nameval tab[], int ntab)
-{
-    int low, high, mid, cmp;
-    int tab, ntab, name;
-    char htmlchars;
-    typedef struct Nameval Nameval;
-    struct Nameval {
-        char    *name;
-        int     value;
-    };
+        
     Nameval htmlchars[] = {
         "AElig",    0x00c6,
         "Aacute",   0x00c1,
@@ -30,12 +22,22 @@ int lookup(char *name, Nameval tab[], int ntab)
         "zeta",     0x03b6,
     };
 
+    half = lookup("zeta", htmlchars, NELEMS(htmlchars));
+    printf("%d\n", half);
+    printf("The HTML table has %d words\n", NELEMS(htmlchars));
+}
+
+/* lookup: binary search for name in tab; return index */
+int lookup(char *name, Nameval tab[], int ntab)
+{
+    int low, high, mid, cmp;
+
     low = 0;
     high = ntab - 1;
     while (low <= high) {
         mid = (low + high) / 2;
         cmp = strcmp(name, tab[mid].name);
-        if (cmp > 0)
+        if (cmp < 0)
             high = mid - 1;
         else if (cmp > 0)
             low = mid + 1;
