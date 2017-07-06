11ec7038554bb21b681875a1c6fb39844d1d9eee
sodua <cthildreth@gmail.com>
2017-07-06 14:50:50 -0700

    lowercased struct so strcmp works

---
+++ bin-diag.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "hcodes.h"
+
+#define NELEMS(array) (sizeof(array) / sizeof(array[0]))
+int main(int argc, char **argv)
+{
+    int half;
+    half = lookup("clockwisecontourintegral", htmlchars, NELEMS(htmlchars));
+    printf("%d\n", half);
+    printf("The HTML table has %d words\n", NELEMS(htmlchars));
+}
+
+/* lookup: binary search for name in tab; return index */
+int lookup(char *name, Nameval tab[], int ntab)
+{
+    int low, high, mid, cmp;
+
+    low = 0;
+    high = ntab - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        printf("mid now %d\n", mid);
+        cmp = strcmp(name, tab[mid].name);
+        printf("comparison value: %d\n", cmp);
+        if (cmp < 0) {
+            high = mid - 1;
+            printf("searching down, high=%d", high);
+            printf("\n");
+        }
+        else if (cmp > 0) {
+            low = mid + 1;
+            printf("searching up, low=%d", low);
+            printf("\n");
+        }
+        else /*found match */ {
+            printf("found! index=%d", mid);
+            printf("\n");
+            return mid;
+        }
+    }
+    return -1; /* no match */
+}
