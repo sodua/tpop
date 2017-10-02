28f47f5cfabc03a1529e772a02ebaed1019ade9c
CT Hildreth <cthildreth@gmail.com>
2017-10-02 18:15:45 -0400

    word length histogram getting close

--- tcpl/1-12.c
+++ tcpl/1-12.c
@@ -11,7 +11,7 @@ int main(void)
             while ((c = getchar()) == ' ' || c == '\n' || c == '\t') {
                 continue;
             }
-       }
-       putchar(c);
+        }
+        putchar(c);
     }
 }
---
+++ tcpl/1-13.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/* Write a program to print a histogram of the lengths of words in
+ * its input. It is easy to draw the histogram with the bars horizontal;
+ * a veritcal orientation is more challenging. */
+
+int main(void)
+{
+    int c, i, nchar;
+    int wlen[10];
+
+    nchar = 0;
+    for (i = 0; i < 10; i++)
+            wlen[i] = 0;
+
+    while ((c = getchar()) != EOF) {
+        nchar = 0;
+        if (c == ' ' || c == '\n' || c == '\t')
+            wlen[nchar] += 1;
+        while ((c = getchar()) != ' ' && c != '\n' && c != '\t') {
+            ++nchar;
+            printf("%d\n", nchar);
+        }
+        wlen[nchar] += 1;
+        for (i = 0; i < 10; ++i)
+            printf("words of length %d: %d\n", i, wlen[i]);
+    }
+}
