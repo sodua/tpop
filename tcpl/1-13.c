a72b6cfc64535f57344f69fc5f1b1f607eddaee7
CT Hildreth <cthildreth@gmail.com>
2017-10-02 18:41:37 -0400

    word histogram working

--- tcpl/1-13.c
+++ tcpl/1-13.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-    int c, i, nchar;
+    int c, i, j, nchar;
     int wlen[10];
 
     nchar = 0;
@@ -14,15 +14,19 @@ int main(void)
             wlen[i] = 0;
 
     while ((c = getchar()) != EOF) {
-        nchar = 0;
+        nchar = 1;
         if (c == ' ' || c == '\n' || c == '\t')
-            wlen[nchar] += 1;
+            continue;
         while ((c = getchar()) != ' ' && c != '\n' && c != '\t') {
             ++nchar;
             printf("%d\n", nchar);
         }
         wlen[nchar] += 1;
-        for (i = 0; i < 10; ++i)
+        for (i = 0; i < 10; ++i) {
             printf("words of length %d: %d\n", i, wlen[i]);
+            for (j = 0; j < wlen[i]; ++j)
+                printf("|");
+            printf("\n");
+        }
     }
 }
