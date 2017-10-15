71539c7ca06293e777a6a9714a6fd98e6fdf407b
CT Hildreth <cthildreth@gmail.com>
2017-10-15 13:51:52 -0400

    program that prints all input lines longer than 80 characters

--- tcpl/test.c
+++ tcpl/test.c
@@ -2,5 +2,12 @@
 
 int main(void)
 {
-    printf("%d %d\n", '0', '9');
+    int p;
+    int n = 4;
+    int base = 2;
+
+    for(p = 1; n > 0; --n) {
+        p = p * base;
+        printf("n: %d p: %d base: %d\n", n, p, base);
+    }
 }
