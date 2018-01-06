9428991d2c7497158ab1a82424fdddb7c59a4971
CT Hildreth <cthildreth@gmail.com>
2018-01-06 15:31:53 -0500

    detab program done

---
+++ tcpl/1-20.c
@@ -0,0 +1,20 @@
+/* Write a program detab that replaces tabs in the input with the
+ * proper number of blanks to space to the next tab stop. Assume a fixed set of
+ * tab stops, say every n columns. Should n be a variable or a symbolic parame-
+ * ter? */
+#include <stdio.h>
+#define TAB_SIZE 4
+
+int main(void)
+{
+    int c, i;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            for (i = 0; i < TAB_SIZE; ++i)
+                putchar(' ');
+        }
+        if (c != '\t')
+            putchar(c);
+    }
+}
