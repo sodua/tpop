8d6aed277e664b7982daf382f2cb97713b9f29ea
CT Hildreth <cthildreth@gmail.com>
2018-03-03 16:17:58 -0500

    line counting function added

--- 322/assn2/csc322a2.c
+++ 322/assn2/csc322a2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void readfile(int numlines, char *line, FILE** readFile);
+int countlines(char *fileName, FILE** countFile);
 
 int main(int argc, char *argv[])
 {
-    FILE *fp_in, *fp_out;
-    char ch;
+    FILE *fp_in, *fp_count;
     int lines = 0;
 
     if (argc < 2)
@@ -18,43 +18,33 @@ int main(int argc, char *argv[])
     {
         fp_in = fopen(argv[1], "r");
         
-        while (!feof(fp_in))
-        {
-            ch = fgetc(fp_in);
-            if (ch == '\n')
-            {
-                lines++;
-            }
-        }
-
-        fclose(fp_in);
-        
-        fp_in = fopen(argv[1], "r");
         if (fp_in == NULL)
         {
             printf("Error: Cannot open input file [%s].\n", argv[1]);
             return 1;
         }
 
-       
+        
+        if (fp_in == NULL)
+        {
+            printf("Error: Cannot open input file [%s].\n", argv[1]);
+            return 1;
+        }
+
+        fp_count = fopen(argv[1], "r");
+        lines = countlines(argv[1], &fp_count); 
+
         readfile(lines, argv[1], &fp_in);
     }
 
     if (argc > 2)
     {
-
         fp_in = fopen(argv[1], "r");
-        fp_out = fopen(argv[2], "w");
+        /* fp_out = fopen(argv[2], "w"); */
 
         if (fp_in == NULL)
         {
             printf("Error: Cannot open input file [%s].\n", argv[1]);
-            return 1;
-        }
-        
-        if (fp_out == NULL)
-        {
-            printf("Error: Cannot open output file [%s].\n", argv[2]);
             return 1;
         }
         
@@ -84,3 +74,19 @@ void readfile(int numlines, char *fileName, FILE** readFile)
     fclose(*readFile);
 }
 
+int countlines(char *fileName, FILE** countFile)
+{
+    char ch;
+    int lines = 0;
+    
+    while (!feof(*countFile))
+    {
+        ch = fgetc(*countFile);
+        if (ch == '\n')
+        {
+            lines++;
+        }
+    }
+
+    return lines;
+}
