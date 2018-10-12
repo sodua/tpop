0af096a0916c8fbfeb5e5e7a113c1dcbb33aa14c
Charles Hildreth <cthildreth@gmail.com>
2018-10-11 20:27:22 -0400

    adding 431 scheduling project

---
+++ 431/Project2_Code.py
@@ -0,0 +1,125 @@
+import copy
+
+class Process:
+
+    def __init__(self,pid,bt,at=0,pr=0):
+        self.pid = pid
+        self.burst_time = bt
+        self.arrival_clock_time = at
+        self.priority = pr
+        self.remaining_burst_time = bt
+
+    def __str__(self):
+        return "Process (PID="+str(self.pid)+")"
+
+    def sim_run(self):
+        self.remaining_burst_time -= 1
+
+    def is_done(self):
+        return self.remaining_burst_time == 0
+
+def getPriority(process):
+    return process.priority
+
+def getBurstTime(process):
+    return process.burst_time
+
+def NP_PRIORITY(ready_queue):
+    rq_copy = ready_queue.copy()
+    sorted_rq = sorted(rq_copy, key=lambda x: x.priority)
+    rq_copy = FCFS(sorted_rq)
+    return rq_copy
+
+def SJF(ready_queue):
+    rq_copy = ready_queue.copy()
+    sorted_rq = sorted(rq_copy, key=lambda x: x.burst_time)
+    rq_copy = FCFS(sorted_rq)
+    return rq_copy
+
+def RR(ready_queue, quantum):
+    rq_copy = ready_queue.copy()
+    USER_MODE, KERNEL_MODE = 0,1
+    mode = KERNEL_MODE
+    running_process = None
+    clock_time = 0
+    my_quantum = quantum
+    process_count = len(rq_copy)
+    i = 0
+
+    while ready_queue:
+        if mode == USER_MODE:
+            if my_quantum >= getBurstTime(running_process):
+                my_quantum -= 1
+                clock_time += 1
+                running_process.sim_run()
+                if running_process.is_done():
+                    mode = KERNEL_MODE
+            elif my_quantum < getBurstTime(running_process):
+                if i < process_count:
+                    running_process = ready_queue[++i]
+                elif i >= process_count:
+                    i = 0
+                    running_process = ready_queue[0]
+        elif mode == KERNEL_MODE:
+            if running_process == None:
+                running_process = ready_queue[0]
+            if running_process.is_done():
+                running_process.finish_clock_time = clock_time
+                del ready_queue[0]
+                if ready_queue:
+                    running_process = ready_queue[0]
+            mode = USER_MODE
+    return rq_copy
+
+
+def FCFS(ready_queue):
+    rq_copy = ready_queue.copy()
+    USER_MODE, KERNEL_MODE = 0,1
+    mode = KERNEL_MODE
+    running_process = None
+    clock_time = 0
+
+    while ready_queue:
+
+        if mode == USER_MODE:
+            clock_time += 1
+            running_process.sim_run()
+            if running_process.is_done():
+                mode = KERNEL_MODE
+        elif mode == KERNEL_MODE:
+            if running_process == None:
+                running_process = ready_queue[0]
+            if running_process.is_done():
+                running_process.finish_clock_time = clock_time
+                del ready_queue[0]
+                if ready_queue:
+                    running_process = ready_queue[0]
+            mode = USER_MODE
+    return rq_copy
+
+def printStats(process_list):
+    total_turnaround_t, total_wait_t = 0, 0
+    for p in process_list:
+        print(p,end=': ')
+        turnaround_t = p.finish_clock_time - p.arrival_clock_time
+        total_turnaround_t += turnaround_t
+        wait_t = turnaround_t - p.burst_time
+        total_wait_t += wait_t
+        print("turnaround time = "+str(turnaround_t)+", wait time = "+str(wait_t))
+    n = len(process_list)
+    avg_turnaround_t = round( total_turnaround_t / n, 2)
+    avg_wait_t = round( total_wait_t / n, 2)
+    print("\naverage turnaround time = "+str(avg_turnaround_t) +", average wait time = "+str(avg_wait_t))
+
+READY_QUEUE = [ Process(1,10,pr=3), Process(2,8,pr=1), Process(3,4,pr=2) ]
+
+print("\n\nFCFS SCHEDULING")
+printStats( FCFS(copy.deepcopy(READY_QUEUE)) )
+print("\n\nNP_PRIORITY SCHEDULING")
+printStats( NP_PRIORITY(copy.deepcopy(READY_QUEUE)) )
+print("\n\nSJF SCHEDULING")
+printStats( SJF(copy.deepcopy(READY_QUEUE)) )
+print("\n\nRR SCHEDULING")
+printStats( RR(copy.deepcopy(READY_QUEUE), 4) )
+
+
