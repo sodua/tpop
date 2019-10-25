26f0b5c21287fce152e5e4c6725f56f2013d681d
Charles Hildreth <cthildreth@gmail.com>
2019-10-25 14:18:49 -0400

    ex. 4-6 done?

--- tcpl/4-3.c
+++ tcpl/4-3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>     /* for atof() */
+#include <math.h>
 
 #define MAXOP   100     /* max size of operand or operator */
 #define NUMBER  '0'     /* signal that a number was found */
@@ -21,13 +22,13 @@ int pswap(void);
 int pdup(void);
 int pclear(void);
 int neg = 0;
-int opcount = 0;
+int subtract = 0;
 int skip_print = 0;
 /* reverse Polish calculator */
 int main(void)
 {
     int type, dividend, divisor, mod;
-    double op2;
+    double op2, var[26], used[26], recent;
     char s[MAXOP];
 
     while ((type = getop(s)) != EOF) {
@@ -35,13 +36,12 @@ int main(void)
             case NUMBER:
                if (neg == 0) {
                     push(atof(s));
-                    opcount++;
                 } 
                else if (neg == 1) {
                     push(atof(s) * -1);
-                    opcount++;
                     neg = 0;
                 }
+                subtract = 0;
                 break;
             case '+':
                 push(pop() + pop());
@@ -61,11 +61,7 @@ int main(void)
                 push(pop() * op2);
                 break;
             case '-':
-                printf("OPCOUNT: %d\n", opcount);
-                if (opcount == 2) {
-                    op2 = pop();
-                    push(pop() - op2);
-                }
+                subtract = 1;
                 break;
             case '/':
                 op2 = pop();
@@ -73,6 +69,9 @@ int main(void)
                     push(pop() / op2);
                 else
                     printf("error: zero divisor\n");
+                break;
+            case 'E':
+                push(exp(pop()));
                 break;
             case 'C':
                 pclear();
@@ -91,17 +90,36 @@ int main(void)
                 pswap();
                 skip_print = 1;
                 break;
+            case 'R':
+                push(recent);
+                break;
             case '\n':
+                if (subtract == 1) {
+                    op2 = pop();
+                    push(pop() - op2);
+                }
                 if (!skip_print && sp > 0) {
                     printf("\t%.8g\n", ptop());
+                    recent = ptop();
                     neg = 0;
-                    opcount = 0;
                 }
                 else {
                     skip_print = 0;
                 }
-                /*for (int i=0; i<=sp; i++)
-                    printf("\t\t%g is value %d\n", val[i], i);*/
+                break;
+            default:
+                if (isalpha(type) && used[type-97] == 1) {
+                    push(var[type-97]);
+                    subtract = 0;
+                }
+                else if (isalpha(type) && used[type-97] != 1) {
+                    //printf("variable: %g saved as %c\n", ptop(), type);
+                    var[type-97] = ptop(); 
+                    used[type-97] = 1;
+                    subtract = 0;
+                }
+                else
+                    printf("error: unknown command %s\n", s);
                 break;
         }
     }
@@ -164,7 +182,7 @@ int getop(char s[])
         ;
     s[1] = '\0';
     if (!isdigit(c) && c != '.') {
-        if (c == '-' && opcount < 2)
+        if (c == '-')
             neg = 1;
         return c;   /* not a number */
     }
